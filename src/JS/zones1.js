55f9ebc233a589b7aeafcc2c00b04f08fdc259e0
ls25047 <ls_25047@mail.ru>
2023-03-17 04:40:10 +0300

    refactoring

--- src/JS/zones1.js
+++ src/JS/zones1.js
@@ -4,11 +4,19 @@
 import { zones } from '../main'
 export class Zones {
     constructor(zones, images) {
+        this.initData(zones, images)
+        this.initImages()
+        this.generateImages()
+        window.setInterval(this.randomImages, 1000)
+    }
+
+    initData() {
         this.zones = zones
         this.images = images
         this.imageActivelist = []
+    }
 
-        const $zones = document.querySelector('.zones')
+    initImages() {
         for (let i = 0; i < 6; i++) {
             const objImg = {
                 time: 0,
@@ -18,9 +26,13 @@ export class Zones {
                 y: 0,
             }
             this.imageActivelist.push(objImg)
-            $zones.insertAdjacentHTML('afterbegin', `<img id="img${i}" class="hide"></img>`)
         }
-        window.setInterval(this.randomImages, 1000)
+    }
+
+    generateImages() {
+        const $zones = document.querySelector('.zones')
+        this.imageActivelist.forEach((_, index) =>
+            $zones.insertAdjacentHTML('afterbegin', `<img id="img${index}" class="hide"></img>`))
     }
 
     randomImages() {
@@ -29,51 +41,64 @@ export class Zones {
         for (let index = 0; index < zones.imageActivelist.length; index++) {
             const element = zones.imageActivelist[index]
             if (element.time > 5000) {
-                const $img = document.querySelector(`#img${index}`)
-                $img.classList.add('hide')
-                element.time = 0
-                element.active = false
-                element.x = 0
-                element.y = 0
+                hideImg(index)
+                setProperties(element, 0, false, 0, 0)
                 continue
             }
-            if (element.time === 0 && wasModified < maxCount) {
-                let randomImg
-                while (true) {
-                    let isOk = true
-                    const num = zones.getRandom(0, zones.images.length)
-                    randomImg = zones.images[num]
-                    for (let i = 0; i < zones.imageActivelist.length; i++) {
-                        if (zones.imageActivelist[i].imageN === num) {
-                            isOk = false
-                            break
-                        }
-                    }
-                    if (isOk === true) {
-                        element.imageN = num
-                        break
-                    }
-                }
-                const $img = document.querySelector(`#img${index}`)
-                const zone = zones.zones[randomImg.zoneId - 1]
-                const activeZone = zone.length < 1 ? zone[0] : zone[zones.getRandom(0, zone.length)]
+            if (element.time === 0 && wasModified < maxCount) {   
+                const randomImg = getImgNumber(element);
+                const activeZone = zones.getZoneById(randomImg.zoneId, zones.zones)
                 const x = zones.getCorrectCoordinates(activeZone.x1, activeZone.x2, 'x', zones.imageActivelist)
                 if (x === -1) continue
                 const y = zones.getCorrectCoordinates(activeZone.y1, activeZone.y2, 'y', zones.imageActivelist)
                 if (y === -1) continue
-                element.x = x
-                element.y = y
-                element.time = 1
-                element.active = true
-                wasModified += 1
-                $img.src = randomImg.src
-                $img.style.left = x + 'px'
-                $img.style.top = y + 'px'
-                $img.classList.remove('hide')
+                setProperties(element, 1, true, x, y)
+                showImg(index, randomImg.src, x, y)
+                wasModified++
             }
-            if (element.active) {
-                element.time += 1000
+            element.active && (element.time += 1000)
+        }
+    }
+
+    getImgNumber(element) {
+        let randomImg
+        while (true) {
+            let isOk = true
+            const num = zones.getRandom(0, zones.images.length)
+            randomImg = zones.images[num]
+            for (let i = 0; i < zones.imageActivelist.length; i++) {
+                if (zones.imageActivelist[i].imageN === num) {
+                    isOk = false
+                    break
+                }
             }
+            if (isOk === true) {
+                element.imageN = num
+                break
+            }
+        }
+        return randomImg
+    }
+
+    setProperties(element, time, active, x, y) {
+        element.time = time
+        element.active = active
+        element.x = x
+        element.y = y
+    }
+
+    hideImg(index) {
+        const $img = document.querySelector(`#img${index}`)
+        $img.classList?.add('hide')
+    }
+
+    showImg(index, src, x, y) {
+        const $img = document.querySelector(`#img${index}`)
+        if ($img) {
+            $img.src = src
+            $img.style.left = x + 'px'
+            $img.style.top = y + 'px'
+            $img.classList.remove('hide')
         }
     }
 
@@ -93,6 +118,15 @@ export class Zones {
             }
         }
         return -1
+    }
+
+    getZoneById(id, zonesList) {
+        const zone = zonesList[id - 1]
+        if (zone.length < 1) {
+            return zone[0]
+        } else {
+            return zone[zones.getRandom(0, zone.length)]
+        }
     }
 
     getRandom(min, max) {
